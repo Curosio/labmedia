a5a7337925dc74675e72700d4fd7b19894b303f1
Mikhail Mikhailov <mikhailoff@inbox.ru>
2019-01-17 23:30:53 +0300

    ADD: ALL Datas

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 // Get the modal
 $( function() {
 
-	$(".close").click(function(){
+	$(".close, .let-reset").click(function(){
 		$("#labmedia-modal").css("display", "none");
+		$(".content-json").html("");
 	});
 
 
@@ -29,20 +30,49 @@ $( function() {
 			            },
 			            complete: function(resp) {
 			            	console.log(resp.responseJSON);
+			            	$modal_data="";
+
 				            if($data_url == "/data/persons.json"){
 				            	$modal_data = "";
 				            	resp.responseJSON.forEach(function(item, i, arr) {
 				            		$number = i+1;
-				            		$modal_data = $modal_data + "<span class'id-person' style='display:none;'>"+item.id+"</span>" + $number + " - " + item.lastname + " " + item.middlename + " " + item.firstname + " (" + item.birthday + ")<hr/>";
+				            		$modal_data = $modal_data + "<span class'id-person' style='display:none;'>"+item.id+"</span> " + $number + " - " + item.lastname + " " + item.middlename + " " + item.firstname + " (" + item.birthday + ")<hr/>";
 
 				            	});
 				            	$(".content-json").html($modal_data);
 				            }
 
+				            if($data_url == "/data/positions.json"){
+				            	$modal_data = "";
+				            	resp.responseJSON.forEach(function(item, i, arr) {
+				            		$number = i+1;
+				            		$modal_data = $modal_data + "<span class'id-position' style='display:none;'>"+item.id+"</span> " + $number + " - " + item.name + " (" + item.min_age + " - " + item.max_age + " лет)<hr/>";
 
+				            	});
+				            	$(".content-json").html($modal_data);
+				            }
 
+  							if($data_url == "/data/orgs.json"){
+				            	$modal_data = "";
+				            	resp.responseJSON.forEach(function(item, i, arr) {
+				            		$number = i+1;
+				            		$modal_data = $modal_data + "<span class'id-position' style='display:none;'>"+item.id+"</span> " + $number + " - " + item.name + " (" + item.country + ") <hr/>";
 
-			            
+				            	});
+				            	$(".content-json").html($modal_data);
+				            }
+
+			            	
+			            	if($data_url == "/data/subs.json"){
+				            	$modal_data = "";
+				            	resp.responseJSON.forEach(function(item, i, arr) {
+				            		$number = i+1;
+				            		$modal_data = $modal_data + "<span class'id-position' style='display:none;'>"+item.id+"</span> " + $number + " - " + item.name + "<span class'id-position' style='display:none;'>" + item.org_id + "</span><hr/>";
+
+				            	});
+				            	$(".content-json").html($modal_data);
+				            }
+
 
 
 
