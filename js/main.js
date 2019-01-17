e498a5f9b7b6261752367cafd4e6a86421ccad11
Mikhail Mikhailov <mikhailoff@inbox.ru>
2019-01-17 18:24:03 +0300

    ADD: Modal Window

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// Get the modal
+$( function() {
+	$(".let-select").click(function(){
+		$("#labmedia-modal").css("display", "block");
+	});
+
+	$(".close").click(function(){
+		$("#labmedia-modal").css("display", "none");
+	});
+
+
+
+});
+
+
